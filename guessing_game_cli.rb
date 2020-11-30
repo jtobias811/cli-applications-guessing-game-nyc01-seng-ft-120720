de242aa59caaf41945ab35d701e5d25f0e66feb3
Jacob Tobias <jtobias811@gmail.com>
2020-11-30 16:48:47 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -6,12 +6,10 @@ def run_guessing_game
 
   if input == number.to_s
     puts "You guessed the correct number!"
+  elsif input == "exit"
+    puts "Goodbye!"
   else
     puts "Sorry! The computer guessed #{number}."
-  end
-
-  if input == "exit"
-    puts "Goodbye!"
   end
 
 end
